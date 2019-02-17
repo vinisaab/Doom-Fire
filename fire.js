ed5012072c2f7daaf19431ca76fac51b8ea4225a
Vinicius Saab <viniciusmsaab@gmail.com>
2019-02-17 09:02:14 -0300

    Add Debug Button

--- fire.js
+++ fire.js
@@ -1,14 +1,14 @@
 //constantes
 const firePixelArray = []
-const fireWidth = 42
-const fireHeight = 42
+let fireWidth = 42
+let fireHeight = 42
 const fireColorsPalette = [{"r":7,"g":7,"b":7},{"r":31,"g":7,"b":7},{"r":47,"g":15,"b":7},{"r":71,"g":15,"b":7},{"r":87,"g":23,"b":7},{"r":103,"g":31,"b":7},{"r":119,"g":31,"b":7},{"r":143,"g":39,"b":7},{"r":159,"g":47,"b":7},{"r":175,"g":63,"b":7},{"r":191,"g":71,"b":7},{"r":199,"g":71,"b":7},{"r":223,"g":79,"b":7},{"r":223,"g":87,"b":7},{"r":223,"g":87,"b":7},{"r":215,"g":95,"b":7},{"r":215,"g":95,"b":7},{"r":215,"g":103,"b":15},{"r":207,"g":111,"b":15},{"r":207,"g":119,"b":15},{"r":207,"g":127,"b":15},{"r":207,"g":135,"b":23},{"r":199,"g":135,"b":23},{"r":199,"g":143,"b":23},{"r":199,"g":151,"b":31},{"r":191,"g":159,"b":31},{"r":191,"g":159,"b":31},{"r":191,"g":167,"b":39},{"r":191,"g":167,"b":39},{"r":191,"g":175,"b":47},{"r":183,"g":175,"b":47},{"r":183,"g":183,"b":47},{"r":183,"g":183,"b":55},{"r":207,"g":207,"b":111},{"r":223,"g":223,"b":159},{"r":239,"g":239,"b":199},{"r":255,"g":255,"b":255}]
-
+let debug = false
 
 //Funcao que inicializa o codigo
 function start(){
     CreateFireDataStructure();
-    console.log(firePixelArray);
+    //console.log(firePixelArray);
     createFireSource();
     RenderFire();
 
@@ -64,7 +64,7 @@ function updateFireIntensityPerPixel(currentPixelIndex) {
 }
 
 function RenderFire(){
-    const debug = false
+    
     //monta a tabela no hmtl para criar o fogo
     let html = '<table cellpadding=0 cellspacing=0>'
 
@@ -79,15 +79,16 @@ function RenderFire(){
             const pixelIndex = column + (fireWidth * row)
             //grava o array de intensidade
             const fireIntensity = firePixelArray[pixelIndex]
+            const color = fireColorsPalette[fireIntensity]
+            const colorString = `${color.r},${color.g},${color.b}`
             
             if (debug === true) {
                 html += '<td>'
                 html +=  `<div class="pixel-index">${pixelIndex} </div>`  
-                html += fireIntensity
+                html += `<div style="color: rgb(${colorString})">${fireIntensity}</div>`
                 html += '</td>'
             }else{
-                const color = fireColorsPalette[fireIntensity]
-                const colorString = `${color.r},${color.g},${color.b}`
+
                 html += `<td class="pixel" style="background-color: rgb(${colorString})"> </td>`
             }
 
@@ -125,6 +126,26 @@ function createFireSource() {
         firePixelArray[pixelIndex] = 36
         
     }
+}
+
+function setDebug(){
+    /*
+        Ativa ou desativa o modo de debug
+    */
+    
+    if (debug === false) {
+        fireWidth = 35
+        fireHeight = 25
+        debug = true
+    } else {
+        fireWidth = 42
+        fireHeight = 42
+        debug = false
+    }
+
+    CreateFireDataStructure();
+    //console.log(firePixelArray);
+    createFireSource();
 }
 
 start()
